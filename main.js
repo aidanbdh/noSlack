06f469fc0a9fb71d66a9c297d05fcc6e57e66a30
Aidan Harvey <aidanharveydev@sarahs-macbook.attlocal.net>
2017-04-17 23:06:48 -0700

    Mentions need fixing #3

--- main.js
+++ main.js
@@ -8,20 +8,42 @@ const rtm = new RtmClient(botToken, {
 })
 
 let contact
+let mainUser
+let self
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(data) {
-  console.log(`Sucessfully logged in here is some data ${data}`)
+  console.log(`Sucessfully logged in as ${data.self.name}`)
+  self = data.self.id
 })
 
 rtm.start()
 
-rtm.once(RTM_EVENTS.MESSAGE, function({text, channel}) {
+rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
   if(text.toLowerCase() !== 'initialize bot') return
   const mainChannel = channel
+  mainUser = user
   rtm.sendMessage(`Hello I am Slack Without Slack, your bot for easily communicating with people who don't have slack! Please respond with a phone number or email address to get started.`, channel)
-  rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel }) {
-    if(channel !== mainChannel) return
+  rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
+    if(channel !== mainChannel || user !== mainUser) return
     contact = text
-    rtm.sendMessage('Thanks! You can now @mention me (the bot) and the message will be sent to whoever you are trying to reach!', channel)
+    rtm.sendMessage('Thanks! You can now @mention me (the bot) and the message will be sent to whoever you are trying to reach! Mention me and add "botInfo" if you want to edit this information at any time!', channel)
+    //Mentions
+    rtm.on(RTM_EVENTS.MESSAGE, function({ text, channel }) {
+      if(text.indexOf(`${self}`) === -1 || text.indexOf('botInfo') === -1) return
+        rtm.sendMessage(`My contact info is ${contact}. If you wish to change it, reply 'Edit'.`, channel)
+        rtm.once(RTM_EVENTS.MESSAGE, function({ user, text, channel }) {
+          if(text.toLowerCase() !== 'edit') return
+          if(user !== mainUser) {
+            rtm.sendMessage(`Only the user that set up this bot can edit the contact information.`, channel)
+          } else {
+            rtm.sendMessage('What would you like the new contact information to be?')
+            rtm.once(RTM_EVENTS.MESSAGE, function({ user, text, channel }) {
+              if(user !== mainUser) return
+              contact = text
+              rtm.sendMessage('Thanks! You can now @mention me (the bot) and the message will be sent to whoever you are trying to reach! Mention me and add "botInfo" if you want to edit this information at any time!', channel)
+            })
+          }
+        })
+    })
   })
 })
