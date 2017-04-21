231fbfb987608963bfe006b83f82aa455f0e8422
Aidan Harvey <aidanharveydev@sarahs-macbook.attlocal.net>
2017-04-20 18:20:46 -0700

    Finished #14

--- main.js
+++ main.js
@@ -1,63 +1,123 @@
 const { RtmClient, CLIENT_EVENTS, MemoryDataStore, RTM_EVENTS } = require('@slack/client')
 const { botToken } = require('./secrets.js')
-const RTM_CLIENT_EVENTS = CLIENT_EVENTS.RTM
 
 const rtm = new RtmClient(botToken, {
   logLevel: 'error',
   dataStore: new MemoryDataStore()
 })
 
-let contact
-let type
-let mainUser
-let self
+const botData = {
+  id: null,
+  initializing: true,
+  populating: true,
+  listening: false,
+  events: 0,
+  admin: {
+    channel: '',
+    user: ''
+  },
+  contact: {
+    phone: 0,
+    email: '',
+    unknown: null
+  }
+}
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(data) {
   console.log(`Sucessfully logged in as ${data.self.name}`)
-  self = data.self.id
+  botData.id = data.self.id
 })
 
 rtm.start()
 
-rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
-  if(text.toLowerCase() !== 'initialize bot') return
-  const mainChannel = channel
-  mainUser = user
-  rtm.sendMessage(`Hello I am Slack Without Slack, your bot for easily communicating with people who don't have slack! Please respond with a phone number or email address to get started.`, channel)
-  rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
-    if(channel !== mainChannel || user !== mainUser) return
-    contact = text
-    rtm.sendMessage('Is this a PHONE or EMAIL?', channel)
-    rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
-      if(channel !== mainChannel || user !== mainUser) return
-      if(text.toLowerCase().indexOf('phone') !== -1) { type = 'phone'
-    } else if (text.toLowerCase().indexOf('email') !== -1) { type = 'email'
-      } else return
-      rtm.sendMessage('Thanks! You can now @mention me (the bot) and the message will be sent to whoever you are trying to reach! Mention me and add "botInfo" if you want to edit this information at any time!', channel)
-      //Mentions
-      rtm.on(RTM_EVENTS.MESSAGE, function({ text, channel }) {
-          if(text.indexOf(`<@${self}>`) === -1 || text.indexOf('botInfo') === -1) return
-          rtm.sendMessage(`My contact info is ${contact}. If you wish to change it, reply 'Edit'.`, channel)
-          rtm.once(RTM_EVENTS.MESSAGE, function({ user, text, channel }) {
-            if(text.toLowerCase() !== 'edit') return
-            if(user !== mainUser) {
-              rtm.sendMessage(`Only the user that set up this bot can edit the contact information.`, channel)
-            } else {
-              rtm.sendMessage('What would you like the new contact information to be?', channel)
-              rtm.once(RTM_EVENTS.MESSAGE, function({ user, text, channel }) {
-                if(user !== mainUser) return
-                contact = text
-                rtm.sendMessage('Is this a PHONE or EMAIL?', channel)
-                rtm.once(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
-                  if(channel !== mainChannel || user !== mainUser) return
-                  if(text.toLowerCase().indexOf('phone') !== -1) { type = 'phone'
-                  } else if (text.toLowerCase().indexOf('email') !== -1) { type = 'email'
-                  } else return
-                  rtm.sendMessage('Thanks! You can now @mention me (the bot) and the message will be sent to whoever you are trying to reach! Mention me and add "botInfo" if you want to edit this information at any time!', channel)              })
-              })
-            }
-          })
-        })
-      })
-  })
+rtm.on(RTM_EVENTS.MESSAGE, function({ text, channel, user }) {
+  text = text.toLowerCase().trim()
+  const send = text => { rtm.sendMessage(text, channel) }
+  const add = () => { botData.events++ }
+  //Messages
+  if(botData.initializing && text.indexOf('start bot')) {
+    switch(botData.events) {
+      //Initial message
+      case 0:
+        send(`Hello! I am Slack without Slack! Please mention me in a direct message to get started.`)
+        add()
+        return;
+      case 1:
+        if(text.toUpperCase().indexOf(`<@${botData.id}>`) === -1) return
+        botData.admin.user = user
+        botData.admin.channel = channel
+        botData.events = 0
+        botData.initializing = false
+        send(`Hello! This is your admin channel, anytime you want to change my settings, just message me here! Send me a message any time to get started!`)
+        return;
+      default:
+        return;
+    }
+  } else if(botData.populating) {
+    switch(botData.events) {
+      //Ask for contact info
+      case 0:
+        send(`Please respond with a phone number or email address to set up contact information.`)
+        add()
+        return;
+      //Confirm info
+      //Ask for contact type
+      case 1:
+        botData.contact.unknown = text
+        send(`Is this a PHONE or EMAIL?`)
+        add()
+        return
+      //Confirm contact info
+      case 2:
+        if(text.indexOf('phone') !== -1) {
+          botData.contact.phone = botData.contact.unknown
+          send(`I have the phone number ${botData.contact.phone} is this right? Reply "no" to retry`)
+          add()
+        } else if (text.indexOf('email') !== -1) {
+          botData.contact.email = botData.contact.unknown
+          send(`I have the email address ${botData.contact.email} is this right? Reply "no" to retry`)
+          add()
+        } else {
+          botData.events = 0
+          if(botData.listening) {
+            send('There was an error setting your contact info. Reply "edit" alone to try again.')
+            return;
+          }
+          send(`There was an error getting your contact information. Please message me at any time to try again.`)
+        }
+        return;
+      //Send OK message
+      case 3:
+        botData.events = 0
+        botData.contact.unknown = null
+        if(text.indexOf('no') !== -1) {
+          botData.contact.phone = 0
+          botData.contact.email = ''
+          send(`Okay. Message me at any time to retry.`)
+          return;
+        }
+        botData.listening = true
+        botData.populating = false
+        send(`Great! You can now send messages to me just by mentioning me! Send "edit" alone in this channel to change or add contact information or "info" alone to see my current information`)
+        return;
+      default:
+        return;
+    }
+  }
+  if(botData.listening) {
+    if(channel === botData.admin.channel && user === botData.admin.user) {
+      if(text === 'edit') {
+        botData.populating = true
+        send('Okay! Send me a message to get started!')
+      }
+      if(text === 'info') {
+        send(`Okay I have the following information. Phone Number : ${botData.contact.phone} and Email Address : ${botData.contact.email}`)
+      }
+    }
+    if(text.toUpperCase().indexOf(`<@${botData.id}>`) === -1) return
+    send(`Oops I'm still in development!`)
+  } else {
+    return;
+  }
+  if(text.toUpperCase().indexOf(`<@${botData.id}>`) === -1) return
 })
